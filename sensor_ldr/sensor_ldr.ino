abdb817449817da3006bcbf2e979fe06094e98df
Ricardo Paiva <werneck.paiva@gmail.com>
2016-01-20 15:23:05 -0200

    First commit

---
+++ sensor_ldr/sensor_ldr.ino
@@ -0,0 +1,70 @@
+#define TRIG_PIN 6
+#define ECHO_PIN 5
+#define SENSOR 0
+#define LED 2
+#define BUZZER_1 8
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  
+  pinMode(LED, OUTPUT);
+}
+
+float readDistance(){
+  int duration;
+  float distances[5];
+  float maxValue=0, minValue=5000;
+
+  for (int i=0; i<5; i++){
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(TRIG_PIN, LOW);
+    int duration = pulseIn(ECHO_PIN, HIGH);
+    distances[i] = (duration / 2) / 29.1;  
+    if (distances[i] < minValue) minValue = distances[i];
+    if (distances[i] > maxValue) maxValue = distances[i];
+    delay(20);
+  }
+  float distance = -1;
+  for (int i=0; i<5; i++){
+    if (distances[i] > minValue && distances[i] < maxValue){
+      distance = distances[i];
+    }
+  }
+  if (distance == -1) distance = distances[0];
+  if (distance < 0) distance = 0;
+  return distance;
+}
+
+void buzz(int buzzTime){
+  tone(BUZZER_1, 1500);
+  delay(100);
+  noTone(BUZZER_1);
+  delay(buzzTime);
+}
+
+  
+void loop() {
+  int lightSign = analogRead(SENSOR);
+  float distance = readDistance();
+
+//  digitalWrite(LED, LOW);
+//  if (lightSign < 15){
+//    digitalWrite(LED, HIGH);
+//  }
+
+  if (distance < 30 && distance > 0){
+    digitalWrite(LED, HIGH);
+//    int buzzTime = (distance < 10)?5:map(distance, 10, 50, 5, 400);
+//    buzz(buzzTime);
+  } else {
+    digitalWrite(LED, LOW);
+  }
+//  Serial.print(lightSign);
+//  Serial.print(",");
+  Serial.println(distance);
+  
+  delay(200);
+}
