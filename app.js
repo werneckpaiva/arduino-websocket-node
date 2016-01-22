dc2e4f4fddd6fe52bae8c3bf7315a3825f41b38a
Ricardo Paiva <werneck.paiva@gmail.com>
2016-01-21 22:24:23 -0200

    Removing unnecessary code

--- app.js
+++ app.js
@@ -1,39 +1,28 @@
 var express = require('express');
-var WebSocketServer = require("ws").Server
-var serialport = require("serialport");
-var SerialPort = serialport.SerialPort;
 var http = require("http")
-
 var app = express();
-var port = process.env.PORT || 5000
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
-server.listen(port)
+server.listen(5000, function(){
+    console.log('App running on port 3000!');
+})
 
+var WebSocketServer = require("ws").Server
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
 
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
 var port = process.argv[2];
 var serialport = new SerialPort(port, {
   baudrate: 9600,
   parser: serialport.parsers.readline("\n")
 });
 
-wss.on("connection", function(ws) {
-  console.log("websocket connection open")
-
-  serialport.on('data', function(data){
-      var value = ""
-      for (p in data) value += data[p];
+serialport.on('data', function(data){
+    var value = ""
+    for (p in data) value += data[p];
+    wss.clients.forEach(function each(ws) {
       ws.send(JSON.stringify(value), function() {  })
-  });
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-  })
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-})
+    });
+});
